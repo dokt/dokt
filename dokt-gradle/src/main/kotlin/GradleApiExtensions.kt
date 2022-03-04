13a50a181c2a9b22369e020c4272528b6296f853
Jukka Papinkivi <jukka@papinkivi.fi>
2022-03-04 16:07:59 +0200

    Update GradleApiExtensions.kt
    Fixed multi-platform detection in #10

--- dokt-gradle/src/main/kotlin/GradleApiExtensions.kt
+++ dokt-gradle/src/main/kotlin/GradleApiExtensions.kt
@@ -1,9 +1,8 @@
 package app.dokt.gradle
 
 import org.gradle.api.*
-import java.io.File
 
-val Project.commonMainDir: File get() = file("src/commonMain")
+val Project.commonMainDir get() = projectDir.resolve("src").resolve("commonMain")
 
 val Project.isRootProject get() = equals(rootProject)
 
